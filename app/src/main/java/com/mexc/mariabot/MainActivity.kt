ca7718dfefefb6de50cedb8a0b117b7492e2fa21
Maria-Bot Builder <malik88anam@gmail.com>
2026-07-18 23:02:50 +0000

    feat: add real native Android WebView wrapper and automatic compilation workflow

---
+++ app/src/main/java/com/mexc/mariabot/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.mexc.mariabot
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        webView = WebView(this)
+        setContentView(webView)
+
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.loadWithOverviewMode = true
+        settings.useWideViewPort = true
+        settings.builtInZoomControls = false
+        settings.displayZoomControls = false
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+
+        // Load the shared development/production URL of Maria-Bot
+        webView.loadUrl("https://ais-pre-i3mqtqg3klqxbqhutlkenp-50095237477.europe-west2.run.app")
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            @Suppress("DEPRECATION")
+            super.onBackPressed()
+        }
+    }
+}
